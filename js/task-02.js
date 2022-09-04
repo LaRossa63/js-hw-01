94ad3151e7892d65732f95761d6042b836184db2
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 18:28:33 +0400

    Задание 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,9 @@
-console.log(2);
+const total = 100;
+const ordered = 50;
+
+const message =
+  total >= ordered
+    ? 'Заказ оформлен, с вами свяжется менеджер'
+    : 'На складе недостаточно товаров!';
+
+console.log(message);
