92cdb45d5d25ac6ec1867fb1a7f51c431d90d384
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 18:21:06 +0400

    fix bug Задание 5, сделал Задание 6

--- js/task-05.js
+++ js/task-05.js
@@ -1,31 +1,35 @@
-const printConsol = (country, price) => {
+const printAccessConsol = (country, price) => {
   console.log(`Доставка в [${country}] будет стоить [${price}] кредитов`);
 };
 
-const input = prompt('Введите страну для доставки');
+const printErrorConsol = () => {
+  console.log('В вашей стране доставка не доступна');
+};
+
+const input = prompt('Введите страну для доставки') || 'cancel ';
 
 switch (input.toLocaleLowerCase()) {
   case 'китай':
-    printConsol('Китай', 100);
+    printAccessConsol('Китай', 100);
     break;
 
   case 'чили':
-    printConsol('Чили ', 250);
+    printAccessConsol('Чили ', 250);
     break;
 
   case 'австралия':
-    printConsol('Австралия', 170);
+    printAccessConsol('Австралия', 170);
     break;
 
   case 'индия':
-    printConsol('Индия', 80);
+    printAccessConsol('Индия', 80);
     break;
 
   case 'ямайка':
-    printConsol('Ямайка', 100);
+    printAccessConsol('Ямайка', 100);
     break;
 
   default:
-    console.log('В вашей стране доставка не доступна');
+    printErrorConsol();
     break;
 }
--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,14 @@
-console.log(6);
+let input = '';
+let totalCount = 0;
+
+while (input !== null) {
+  input = prompt('Введите число!');
+
+  if (input === null) {
+    break;
+  }
+
+  totalCount += Number(input);
+}
+
+console.log(`Общая сумма чисел равна [${totalCount}]`);
