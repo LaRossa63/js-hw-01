ae56e29d13b77287b749ede7a28e1222473cecc6
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 17:47:42 +0400

    Задание 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,31 @@
-console.log(5);
+const printConsol = (country, price) => {
+  console.log(`Доставка в [${country}] будет стоить [${price}] кредитов`);
+};
+
+const input = prompt('Введите страну для доставки');
+
+switch (input.toLocaleLowerCase()) {
+  case 'китай':
+    printConsol('Китай', 100);
+    break;
+
+  case 'чили':
+    printConsol('Чили ', 250);
+    break;
+
+  case 'австралия':
+    printConsol('Австралия', 170);
+    break;
+
+  case 'индия':
+    printConsol('Индия', 80);
+    break;
+
+  case 'ямайка':
+    printConsol('Ямайка', 100);
+    break;
+
+  default:
+    console.log('В вашей стране доставка не доступна');
+    break;
+}
