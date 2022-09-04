f8f16e7fe257c8228cedfa0d1f2eb56e4521c5d7
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 18:30:53 +0400

    Задание 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,12 @@
-console.log(3);
+const ADMIN_PASSWORD = '123';
+let message;
+
+const input = prompt('Введите пароль');
+
+if (input === null) {
+  console.log('Отменено пользователем!');
+} else if (input === ADMIN_PASSWORD) {
+  console.log('Добро пожаловать!');
+} else {
+  console.log('Доступ запрещен, неверный пароль!');
+}
