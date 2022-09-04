661b115b491ab086a62487b1a6d7ace277e69d9e
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 17:40:03 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,21 @@
-console.log(4);
+const credits = 23580;
+const pricePerDroid = 3000;
+
+const processingInput = (text) => {
+  if (text === null) {
+    return 'Отменено пользователем!';
+  }
+
+  const countDroid = Number(text);
+  const totalPrice = pricePerDroid * countDroid;
+
+  return credits > totalPrice
+    ? `Вы купили [${countDroid}] дронов, на счету осталось [${
+        credits - totalPrice
+      }] кредитов.`
+    : 'Недостаточно средств на счету!';
+};
+
+const input = prompt('Сколько дронов Вам нужно ?');
+
+console.log(processingInput(input));
